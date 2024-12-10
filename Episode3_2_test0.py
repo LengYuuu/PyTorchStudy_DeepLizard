153e03650cee425305e1f412158cdaf03f36461f
Fangzhou Tan <3465306929@qq.com>
2024-12-10 14:09:29 +0800

    Part 2: Neural Network Training * Section 1: Data and Data Processing * Section 2: Neural Networks and PyTorch Design * Section 3: Training Neural Networks

--- Episode3_2_test0.py
+++ Episode3_2_test0.py
@@ -47,11 +47,15 @@ network = Network()
 train_loader = torch.utils.data.DataLoader(train_set, batch_size=100)    # step 1: Get batch from the training set.
 optimizer = optim.Adam(network.parameters(), lr=0.01)
 
-batch = next(iter(train_loader))
-images, labels = batch
+for batch in train_loader:  # step 6: Repeat steps 1-5 until one epoch is completed.
+    images, labels = batch
 
-preds = network(images) # step 2: Pass batch to network.
-loss = F.cross_entropy(preds, labels)   # step 3: Calculate the loss(difference between the predicted values and the true values).
+    preds = network(images) # step 2: Pass batch to network.
+    loss = F.cross_entropy(preds, labels)   # step 3: Calculate the loss(difference between the predicted values and the true values).
 
-loss.backward() # step 4: Calculate the gradient of the loss function w.r.t the network's weights.
-optimizer.step()    # step 5: Update the weights using the gradients to reduce the loss.
+    optimizer.zero_grad()
+    loss.backward() # step 4: Calculate the gradient of the loss function w.r.t the network's weights.
+    optimizer.step()    # step 5: Update the weights using the gradients to reduce the loss.
+
+print(loss.item())
+print(get_num_correct(preds, labels))
